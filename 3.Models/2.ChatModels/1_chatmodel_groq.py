b8de0c361308b2b002a577a5451e7aff4f4db86d
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-28 00:49:05 +0530

    small update in codebase

--- 3.Models/2.ChatModels/1_chatmodel_groq.py
+++ 3.Models/2.ChatModels/1_chatmodel_groq.py
@@ -1,9 +1,9 @@
-from langchain_groq import ChatGroq
-from dotenv import load_dotenv
-load_dotenv()
+from langchain_groq import ChatGroq # Import the ChatGroq class from the langchain_groq module
+from dotenv import load_dotenv # Import the load_dotenv function from the dotenv module
+load_dotenv() # Load environment variables from a .env file
 
-chat = ChatGroq(model="llama-3.3-70b-versatile", temperature=0.5)
+chat = ChatGroq(model="llama-3.3-70b-versatile", temperature=0.5) # Create an instance of the ChatGroq class with the specified model and temperature
 
-result = chat.invoke("What is the langchain")
+result = chat.invoke("What is the langchain") # Invoke the model with the specified prompt
 
-print(result.content)
+print(result.content) # print the content from the result
