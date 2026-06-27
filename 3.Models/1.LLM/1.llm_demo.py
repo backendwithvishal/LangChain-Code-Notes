1912310c1fee527e7b0303d3196530df58816a22
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-28 00:46:30 +0530

    comment add in llm_demo file

--- 3.Models/1.LLM/1.llm_demo.py
+++ 3.Models/1.LLM/1.llm_demo.py
@@ -1,9 +1,9 @@
-from langchain_groq import ChatGroq
-from dotenv import load_dotenv
-load_dotenv()
+from langchain_groq import ChatGroq # Import the ChatGroq class from the langchain_groq module
+from dotenv import load_dotenv # Import the load_dotenv function from the dotenv module
+load_dotenv() # Load environment variables from a .env file
 
-llm = ChatGroq(model="llama-3.3-70b-versatile")
+llm = ChatGroq(model="llama-3.3-70b-versatile") # Create an instance of the ChatGroq class with the specified model
 
-res = llm.invoke("What is the capital of Maharashtra?")
+result = llm.invoke("What is the capital of Maharashtra?") # Invoke the model with the specified prompt
 
-print(res.content)
+print(result.content) # Print the content of the response
--- 3.Models/2.ChatModels/1_chatmodel_groq.py
+++ 3.Models/2.ChatModels/1_chatmodel_groq.py
@@ -2,8 +2,8 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 load_dotenv()
 
-chat = ChatGroq(model="llama-3.3-70b-versatile")
+chat = ChatGroq(model="llama-3.3-70b-versatile", temperature=0.5)
 
-result = chat.invoke("What is the capital of Japan?")
+result = chat.invoke("What is the langchain")
 
 print(result.content)
