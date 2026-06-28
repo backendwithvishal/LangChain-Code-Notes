264670269c719d16134a82b5fe4126d0de911398
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-28 21:17:10 +0530

    small update in codebase

--- 3.Models/2.ChatModels/3_chatmodel_hf_api.py
+++ 3.Models/2.ChatModels/3_chatmodel_hf_api.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
-    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
     task="text-generation"
 )
 
