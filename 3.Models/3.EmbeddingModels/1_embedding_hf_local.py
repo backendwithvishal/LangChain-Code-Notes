f56f4ccc6208438fa48e8090b8d40681b0c0696a
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-28 21:40:16 +0530

    code added

---
+++ 3.Models/3.EmbeddingModels/1_embedding_hf_local.py
@@ -0,0 +1,12 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+embedding = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+
+documents = [
+    "Mumbai is the capital of Maharashtra",
+    "Delhi is the capital of India",
+    "Kolkata is the capital of West Bengal",
+]
+
+vector = embedding.embed_documents(documents)
+
+print(str(vector))
