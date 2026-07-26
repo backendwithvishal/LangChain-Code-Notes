e33505914690963703bee32fba287498d9efcbdd
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-26 12:54:48 +0530

    small update in file

--- 4.Prompts/prompt_ui.py
+++ 4.Prompts/prompt_ui.py
@@ -30,23 +30,21 @@ length_input = st.selectbox(
     "Explanation Length",["Short (1-2 paragraphs)","Medium (3-5 paragraphs)","Long (Detailed Explanation)",])
 
 template = PromptTemplate(
-    template="""  """
+    template=""" Please summarize the research paper titled "{paper_input}" with the following specifications:
+Explanation Style: {style_input}  
+Explanation Length: {length_input}  
+1. Mathematical Details:  
+   - Include relevant mathematical equations if present in the paper.  
+   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
+2. Analogies:  
+   - Use relatable analogies to simplify complex ideas.  
+If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
+Ensure the summary is clear, accurate, and aligned with the provided style and length. """
 ) 
 
 if st.button("Summarize"):
 
-    prompt = f"""
-    Summarize the research paper "{paper_input}".
 
-    Use a {style_input} explanation.
-
-    Keep the explanation {length_input}.
-
-    Explain the main idea, important concepts, advantages,
-    limitations, and real-world applications.
-    """
-
-    response = model.invoke(prompt)
 
     st.subheader("Summary")
-    st.write(response.content)
+    st.write()
