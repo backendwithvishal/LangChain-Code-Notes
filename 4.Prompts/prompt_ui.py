cad8ba07f2efac48a04372019a39e133bc471f12
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-27 13:01:43 +0530

    Error fixed in the codebase

--- 4.Prompts/prompt_ui.py
+++ 4.Prompts/prompt_ui.py
@@ -1,14 +1,12 @@
-from langchain_core.prompts.loading import _load_prompt
+import json
 import os
 import streamlit as st
 from dotenv import load_dotenv
+from langchain_core.prompts.loading import _load_prompt
 from langchain_groq import ChatGroq
-from langchain_core.prompts import PromptTemplate
 
-# Load environment variables
 load_dotenv()
 
-# Create the LLM
 model = ChatGroq(
     model="llama-3.3-70b-versatile",
     api_key=os.getenv("GROQ_API_KEY")
@@ -16,29 +14,51 @@ model = ChatGroq(
 
 st.header("📄 Research Tool 🔍")
 
-# Research paper selection
 paper_input = st.selectbox(
-    "Select Research Paper", ["Attention Is All You Need","BERT: Pre-training of Deep Bidirectional Transformers",
-    "GPT-3: Language Models are Few-Shot Learners","Diffusion Models Beat GANs on Image Synthesis",]) 
+    "Select Research Paper",
+    [
+        "Attention Is All You Need",
+        "BERT: Pre-training of Deep Bidirectional Transformers",
+        "GPT-3: Language Models are Few-Shot Learners",
+        "Diffusion Models Beat GANs on Image Synthesis",
+    ],
+)
 
-# Explanation style
 style_input = st.selectbox(
-    "Explanation Style",["Beginner-Friendly","Technical","Code-Oriented","Mathematical",] )
+    "Explanation Style",
+    [
+        "Beginner-Friendly",
+        "Technical",
+        "Code-Oriented",
+        "Mathematical",
+    ],
+)
 
-# Explanation length
 length_input = st.selectbox(
-    "Explanation Length",["Short (1-2 paragraphs)","Medium (3-5 paragraphs)","Long (Detailed Explanation)",])
+    "Explanation Length",
+    [
+        "Short (1-2 paragraphs)",
+        "Medium (3-5 paragraphs)",
+        "Long (Detailed Explanation)",
+    ],
+)
 
-# prompt
-template = _load_prompt('template.json')
+# Load template.json
+with open("template.json", "r", encoding="utf-8") as file:
+    config = json.load(file)
 
-# calling the LLM model
+template = _load_prompt(config)
+
 if st.button("Summarize"):
-    Chain = template | model
-    # fill the placeholder
-    result = Chain.invoke({
-        "paper_input":paper_input,
-        "style_input":style_input,
-        "length_input":length_input,
-    })
+
+    chain = template | model
+
+    result = chain.invoke(
+        {
+            "paper_input": paper_input,
+            "style_input": style_input,
+            "length_input": length_input,
+        }
+    )
+
     st.write(result.content)
