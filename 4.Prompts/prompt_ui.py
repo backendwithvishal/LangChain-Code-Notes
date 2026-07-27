8d57302adb919e60d8cfef98c622e1e781c55b72
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-27 12:41:08 +0530

    small update in code base

--- 4.Prompts/prompt_ui.py
+++ 4.Prompts/prompt_ui.py
@@ -1,3 +1,4 @@
+from langchain_core.prompts.loading import _load_prompt
 import os
 import streamlit as st
 from dotenv import load_dotenv
@@ -28,21 +29,8 @@ style_input = st.selectbox(
 length_input = st.selectbox(
     "Explanation Length",["Short (1-2 paragraphs)","Medium (3-5 paragraphs)","Long (Detailed Explanation)",])
 
-
-template = PromptTemplate(
-    template=""" Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}  
-Explanation Length: {length_input}  
-1. Mathematical Details:  
-   - Include relevant mathematical equations if present in the paper.  
-   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
-2. Analogies:  
-   - Use relatable analogies to simplify complex ideas.  
-If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
-Ensure the summary is clear, accurate, and aligned with the provided style and length. """
-) 
-
-input_variables=["paper_input","style_input","length_input"]
+# prompt
+template = _load_prompt('template.json')
 
 # fill the placeholder
 prompt = template.invoke({
