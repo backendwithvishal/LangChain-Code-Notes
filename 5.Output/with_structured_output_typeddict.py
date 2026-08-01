dec5ed6aaf718a20ffd42bc281d35d282241a72e
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-02 00:20:34 +0530

    small update in code done ✅

--- 5.Output/with_structured_output_typeddict.py
+++ 5.Output/with_structured_output_typeddict.py
@@ -1,6 +1,7 @@
+from typing import Literal
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict, Annotated, Optional
 
 load_dotenv()
 
@@ -9,13 +10,30 @@ model = ChatGroq(
 )
 
 class Review(TypedDict):
-    summery: str
-    sentiment: str
+
+    key_themes: Annotated[list[str], "List down all the key themes of the review which are generally related to product"]
+    summery: Annotated[str, "A brief summary of the review"]
+    sentiment: Annotated[Literal["Postive", "Negative", "Neutral"], "The sentiment of the review either negative, positive or neutral"]
+    pros: Annotated[Optional[list[str]], "Write down all the pros inside a list"]
+    cons: Annotated[Optional[list[str]], "Write down all the cons inside a list"]
 
 structured_model = model.with_structured_output(Review)
 
-result = structured_model.invoke(
-    "The hardware is great but the battery is not good"
+result = structured_model.invoke( """ I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+                                 
+Review by Aryan Sanam """
 )
 
-print(type(result))
+print(result)
+# print(["summary"])
+print(["senriment"])
