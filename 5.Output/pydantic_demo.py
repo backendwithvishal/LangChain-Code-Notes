00b79307773b38b6de26ad8131ba8ac6ab1b1017
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-02 14:37:00 +0530

    Optional attribute added

--- 5.Output/pydantic_demo.py
+++ 5.Output/pydantic_demo.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Student(BaseModel):
 
     name: str = 'Vishal'
+    age: Optional[int] = None
 
 new_student = {}
 
 student = Student(**new_student)
 
-print(student.name)
+print(student)
