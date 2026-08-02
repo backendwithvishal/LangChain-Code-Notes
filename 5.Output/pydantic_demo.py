abcaa8c449f604a26560f68f39a1a481e87b0154
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-02 14:45:09 +0530

    small update in codebase

--- 5.Output/pydantic_demo.py
+++ 5.Output/pydantic_demo.py
@@ -1,12 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr , Field
 from typing import Optional
 
 class Student(BaseModel):
 
     name: str = 'Vishal'
     age: Optional[int] = None
+    cgpa: float = Field(gt=0, lt=10)
+    # email: EmailStr 
 
-new_student = {}
+new_student = {'age':20, 'cgpa': 9 }
 
 student = Student(**new_student)
 
