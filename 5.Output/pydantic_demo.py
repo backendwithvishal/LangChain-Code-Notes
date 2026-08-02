0aca39e1634d904bdb4c276db541687ea52f1802
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-02 14:49:18 +0530

    small update in codebase

--- 5.Output/pydantic_demo.py
+++ 5.Output/pydantic_demo.py
@@ -5,11 +5,13 @@ class Student(BaseModel):
 
     name: str = 'Vishal'
     age: Optional[int] = None
-    cgpa: float = Field(gt=0, lt=10)
+    cgpa: float = Field(gt=0, lt=10, default = 8,description='A decimal value representing the cgpa of the student')
     # email: EmailStr 
 
-new_student = {'age':20, 'cgpa': 9 }
+new_student = {'age':20}
 
 student = Student(**new_student)
 
 print(student)
+
+student_json = student.model_dump_json()
