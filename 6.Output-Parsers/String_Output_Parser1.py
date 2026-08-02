3280ba267a9101a1faf01e2d7f19423f92d96c82
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-03 00:01:06 +0530

    code added in new file

---
+++ 6.Output-Parsers/String_Output_Parser1.py
@@ -0,0 +1,24 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+# First Prompt --> detailed report
+template = PromptTemplate(
+    template="Write a detailed report on {topic}",
+    input_variables=["topic"]
+)
+
+# Second Prompt --> summary
+template1 = PromptTemplate(
+    template="Write a 5 line summary on the following text.\n{text}",
+    input_variables=["text"]
+)
