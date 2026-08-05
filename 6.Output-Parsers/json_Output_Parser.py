001118d12ab888b5e7283e92d7d8ff8ef9af8d03
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-05 23:38:58 +0530

    more code added in file

--- 6.Output-Parsers/json_Output_Parser.py
+++ 6.Output-Parsers/json_Output_Parser.py
@@ -1,3 +1,4 @@
+from String_Output_Parser1 import parser
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
@@ -10,3 +11,12 @@ llm = HuggingFaceEndpoint(
 )
 
 model = ChatHuggingFace(llm=llm)
+
+template = PromptTemplate(
+    template = 'Give me the name, age and city of the fictional person \n {format_instruction}',
+    input_variables = [],
+    partial_variables = {'format_instruction': parser.get_format_instruction()}
+)
+prompt = template.format()
+
+print(prompt)
