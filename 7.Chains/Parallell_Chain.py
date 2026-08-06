eedf715fc32e43f51ea7c50829af138f152f71b2
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-06 21:49:14 +0530

    Small update in codebase

--- 7.Chains/Parallell_Chain.py
+++ 7.Chains/Parallell_Chain.py
@@ -6,23 +6,35 @@ from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
-# Prompt 1
-prompt1 = PromptTemplate(
-    template="Generate a detailed report on {topic}",
-    input_variables=["topic"]
-)
-
-# Prompt 2
-prompt2 = PromptTemplate(
-    template="Generate a 5 point summary from the following text:\n{text}",
-    input_variables=["text"]
-)
-
 # Hugging Face Endpoint
 llm = HuggingFaceEndpoint(
     repo_id="Qwen/Qwen2.5-7B-Instruct",
     task="text-generation"
 )
+
+# LLM
 model = ChatGroq(
     model="llama-3.3-70b-versatile"
 )
+
+# Prompt 1
+prompt1 = PromptTemplate(
+    template="Generate short and single notes from the follwing text \n {text}",
+    input_variables=["text"]
+)
+
+# Prompt 2
+prompt2 = PromptTemplate(
+    template="Generate a 5 short question answer from the following text:\n{text}",
+    input_variables=["text"]
+)
+
+# Prompt 3
+# Final Prompt
+prompt3 = PromptTemplate(
+    template = "Merge the provided notes and quiz into a single doucument. \n notes -> {notes} and quiz -> {quiz}",
+    input_variables = ["notes", "quiz"]
+) 
+
+parser = StrOutputParser()
+
