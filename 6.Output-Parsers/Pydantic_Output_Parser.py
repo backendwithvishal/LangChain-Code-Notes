7cd928858c94a60a372444831082025352a8e33d
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-06 13:24:19 +0530

    small update in codebase

--- 6.Output-Parsers/Pydantic_Output_Parser.py
+++ 6.Output-Parsers/Pydantic_Output_Parser.py
@@ -13,3 +13,22 @@ llm = HuggingFaceEndpoint(
 
 model = ChatHuggingFace(llm=llm)
 
+class Person (BaseModel):
+
+    name: str = Field(description = 'Name of the Person')
+    age: int = Field(gt = 18, description = 'Age of the Person')
+    city: str = Field(description = 'Name of the city the Person belongs to')
+
+parser = PydanticOutputParser(pydantic_object = Person)
+
+template = PromptTemplate(
+    template = 'Generate the name, age and city of the fictional {place} person \n {format_instruction}',
+    input_variables = ['place'],
+    partial_variables = {'format_instruction':parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+final_result = chain.invoke({'place':'Indian'})
+
+print(final_result)
