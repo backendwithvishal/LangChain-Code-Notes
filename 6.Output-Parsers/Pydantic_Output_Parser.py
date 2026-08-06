169a1674a359cc97069ab54991b9f2ef3d62671e
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-06 13:12:17 +0530

    code added in new  file

---
+++ 6.Output-Parsers/Pydantic_Output_Parser.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+# from langchain_classic.output_parsers import PydanticOutputParser
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
