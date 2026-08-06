736f7772599a682e7e05da8de6be3ddbcb040fe9
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-06 17:30:31 +0530

    code added in new file

---
+++ 7.Chains/Sequential_chain.py
@@ -0,0 +1,26 @@
+from langchain_huggingface import ChatHuggingFace
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template = "Generate a detailed report on {topic}",
+    input_variables = ["topic"]
+)
+
+prompt2 = PromptTemplate(
+    template = "Generate a 5 pointer summary from the following text \n {text}",
+    input_variables = ["text"]
+)
+
+model = ChatHuggingFace()
+
+parser = StrOutputParser()
+
+chain = prompt1 | model | parser | prompt2 | model | parser
+
+result = chain.invoke({"topic" : "India's 80 Indenpendace Year"})
+
+print(result)
