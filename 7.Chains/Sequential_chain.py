ba2b18dc05419e27f84a94cfd11b18dc53d81bba
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-06 17:40:18 +0530

    small update codebase

--- 7.Chains/Sequential_chain.py
+++ 7.Chains/Sequential_chain.py
@@ -1,26 +1,42 @@
-from langchain_huggingface import ChatHuggingFace
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
+# Prompt 1
 prompt1 = PromptTemplate(
-    template = "Generate a detailed report on {topic}",
-    input_variables = ["topic"]
+    template="Generate a detailed report on {topic}",
+    input_variables=["topic"]
 )
 
+# Prompt 2
 prompt2 = PromptTemplate(
-    template = "Generate a 5 pointer summary from the following text \n {text}",
-    input_variables = ["text"]
+    template="Generate a 5 point summary from the following text:\n{text}",
+    input_variables=["text"]
 )
 
-model = ChatHuggingFace()
+# Hugging Face Endpoint
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation"
+)
 
+# Chat Model
+model = ChatHuggingFace(llm=llm)
+
+# Output Parser
 parser = StrOutputParser()
 
+# Sequential Chain
 chain = prompt1 | model | parser | prompt2 | model | parser
 
-result = chain.invoke({"topic" : "India's 80 Indenpendace Year"})
+# Invoke Chain
+result = chain.invoke({
+    "topic": "India's 80th Independence Year"
+})
 
 print(result)
+
+graph = chain.get_graph().print_ascii()
