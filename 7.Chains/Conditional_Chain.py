c862b22848677b817248e9c9eb461f185ecb567a
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-07 12:52:15 +0530

    small update in codebase

--- 7.Chains/Conditional_Chain.py
+++ 7.Chains/Conditional_Chain.py
@@ -1,4 +1,5 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+# from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_openrouter import ChatOpenRouter
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
@@ -7,6 +8,7 @@ from langchain_core.runnables import RunnableParallel
 
 load_dotenv()
 
+''' 
 # Hugging Face Endpoint
 llm = HuggingFaceEndpoint(
     repo_id="Qwen/Qwen2.5-7B-Instruct",
@@ -15,8 +17,19 @@ llm = HuggingFaceEndpoint(
 
 # Chat Model
 hf_model = ChatHuggingFace(llm=llm)
+'''
 
+# --------------------------
 # Groq Model
+# --------------------------
 groq_model = ChatGroq(
     model="llama-3.3-70b-versatile"
+)
+
+# --------------------------
+# OpenRouter Model
+# --------------------------
+openrouter_model = ChatOpenRouter(
+    model="meta-llama/llama-3.3-70b-instruct",
+    temperature=0
 )
