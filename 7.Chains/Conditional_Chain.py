c814ecc7a85c6a0bdf90c34c16aa01b1331342de
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-07 13:23:36 +0530

    small update in codebase

--- 7.Chains/Conditional_Chain.py
+++ 7.Chains/Conditional_Chain.py
@@ -7,7 +7,6 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableParallel
 
 load_dotenv()
-
 '''
 Hugging Face Endpoint
 
@@ -17,24 +16,26 @@ llm = HuggingFaceEndpoint(
 )
 
 Chat Model
-
 hf_model = ChatHuggingFace(llm=llm) '''
 
-# --------------------------------------------------
 # Groq Model
-# --------------------------------------------------
 
 groq_model = ChatGroq(
     model="llama-3.3-70b-versatile"
 )
 
-# --------------------------------------------------
 # OpenRouter Model
-# --------------------------------------------------
 
 openrouter_model = ChatOpenRouter(
     model="meta-llama/llama-3.3-70b-instruct",
     temperature=0
 )
+parser = StrOutputParser()
 
-print("Import Successful!")
+prompt1 = PromptTemplate(
+    template = """
+    Review the following feedback and determine if it is positive or negative Feedback: {Feedback}""",
+    input_variables = ["Feedback"]
+)
+
+print(prompt1)
