b9eec4e1fafbfce449d510478a6cf194b9f345c1
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-07 12:41:02 +0530

    code added in new file

---
+++ 7.Chains/Conditional_Chain.py
@@ -0,0 +1,22 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel
+
+load_dotenv()
+
+# Hugging Face Endpoint
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="conversational"
+)
+
+# Chat Model
+hf_model = ChatHuggingFace(llm=llm)
+
+# Groq Model
+groq_model = ChatGroq(
+    model="llama-3.3-70b-versatile"
+)
