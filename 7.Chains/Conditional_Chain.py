ad20c30a62bfec0900ca1725a8cbcb8bc97e92b1
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-07 19:54:24 +0530

    small update in codebase

--- 7.Chains/Conditional_Chain.py
+++ 7.Chains/Conditional_Chain.py
@@ -1,10 +1,14 @@
 # from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.output_parsers import format_instructions
 from langchain_openrouter import ChatOpenRouter
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableParallel
+from langchain_core.runnables import RunnableParallel, RunnableBranch, RunnableLambda 
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import Literal
 
 load_dotenv()
 '''
@@ -32,11 +36,35 @@ openrouter_model = ChatOpenRouter(
 )
 parser = StrOutputParser()
 
+class Feedback(BaseModel):
+    sentiment: Literal['positive', 'negative','neutral'] = Field(description = 'Give the sentiment of the feedback' )
+
+parser1 = PydanticOutputParser(pydantic_object = Feedback)
+
 prompt1 = PromptTemplate(
-    template = "Classify the sentiment of the following feedback text into postive or negative \n {feedback}",
-    input_variables = ["feedback"]
+    template = "Classify the sentiment of the following feedback text into postive or negative \n {feedback} \n {format_instructions}",
+    input_variables = ["feedback"],
+    partial_variables = {'format_instructions':parser1.get_format_instructions()}
 )
 
-classifier_chain = prompt1 | openrouter_model | parser
+classifier_chain = prompt1 | openrouter_model | parser1
 
-print(classifier_chain.invoke({'feedback':'This is a terrible smartphone'}))
+prompt2 = PromptTemplate(
+    template = 'Write an appropiate response to this positive feedback \n {feedback}',
+    input_variables = ['feedback']
+)
+
+prompt3 = PromptTemplate(
+    template = 'Write an appropiate response to this negative feedback \n {feedback}',
+    input_variables = ['feedback']
+)
+
+branch_chain = RunnableBranch(
+    (lambda x:x.sentiment == 'positive', prompt2 | openrouter_model | parser),
+    (lambda x:x.sentiment == 'negative', prompt3 | groq_model | parser),
+    RunnableLambda(lambda x: "could not classify the feedback")
+) 
+
+chain = classifier_chain | branch_chain
+
+print(chain.invoke({'feedback': 'this is a good phone'}))
