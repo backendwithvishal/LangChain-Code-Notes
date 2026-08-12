16468c2c054ac7337339ede72c418e61e81e0b3b
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-12 20:04:47 +0530

    small update in codebase

--- 9.Runnable_Part_II/runnable_passthrough.py
+++ 9.Runnable_Part_II/runnable_passthrough.py
@@ -7,47 +7,29 @@ from langchain_core.runnables import ( RunnableSequence, RunnableParallel, Runna
 load_dotenv()
 
 prompt1 = PromptTemplate(
-    template="Generate a tweet about {topic}",
-    input_variables=["topic"]
+    template='Write a joke about {topic}',
+    input_variables=['topic']
 )
-
 
 model = ChatGroq(
     model="llama-3.1-8b-instant",
     temperature=0.4
 )
 
-prompt2 = PromptTemplate(
-    template="Generate a Linkedin post about {topic}",
-    input_variables=["topic"]
-)
-
 parser = StrOutputParser()
 
-joke_gen_chain = RunnableSequence(
-    prompt1,
-    model,
-    parser
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
 )
 
-linkedin_chain = RunnableSequence(
-    prompt2,
-    model,
-    parser
-)
+joke_gen_chain = RunnableSequence(prompt1, model, parser)
 
 parallel_chain = RunnableParallel({
-    "joke": joke_gen_chain,
-    "explanation": linkedin_chain
+    'joke': RunnablePassthrough(),
+    'explanation': RunnableSequence(prompt2, model, parser)
 })
 
-final_chain = RunnableSequence(
-    RunnablePassthrough(),
-    parallel_chain
-)
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
 
-result = final_chain.invoke({"topic": "Cricket"})
-
-print(result["joke"])
-
-print(result["explanation"])
+print(final_chain.invoke({'topic':'cricket'}))
