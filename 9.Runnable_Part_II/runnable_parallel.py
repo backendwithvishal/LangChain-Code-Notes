1073eaca8405efbbfe748fc8ccf8c5caf58304ce
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-12 13:19:14 +0530

    new file added with code

---
+++ 9.Runnable_Part_II/runnable_parallel.py
@@ -0,0 +1,32 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables.base import RunnableSequence, RunnableParallel
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template = "Generate a tweet about {topic}",
+    input_variables = ['topic']
+)
+
+prompt2 = PromptTemplate(
+    template = "Generate a Linkedin post about {topic}",
+    input_variables = ['topic']
+)
+
+model = ChatGroq(
+    model="llama-3.1-8b-instant",
+    temperature=0.4
+)
+parser = StrOutputParser()
+
+parallel_chain = RunnableParallel({
+    "tweet": RunnableSequence(prompt1, model, parser),
+    "Linedin": RunnableSequence(prompt2, model, parser)
+})
+
+result = parallel_chain.invoke({"topic": "AI"})
+
+print(result)
