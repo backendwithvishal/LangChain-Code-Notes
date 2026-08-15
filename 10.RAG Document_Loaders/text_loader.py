9d1bb2fd99ae8b7ba05475dd424b1cdf7b6cfd62
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-15 19:50:01 +0530

    small update in codebase

--- 10.RAG Document_Loaders/text_loader.py
+++ 10.RAG Document_Loaders/text_loader.py
@@ -1,7 +1,29 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGroq(
+    model = 'llama-3.3-70b-versatile'
+)
+
+prompt = PromptTemplate(
+    template = 'Write a summary for the following poem - \n {poem}',
+    input_variables = ['poem']
+)
 
 loader = TextLoader('cricket.txt', encoding='utf-8')
 docus = loader.load()
-# print(docus)
-print(type(docus))
-# print(docus[0].page_content)
+
+# print(len(docus))
+
+# print(type(docus))
+
+# print(docus[0])
+
+print(docus[0].page_content)
+
+print(docus[0].metadata)
