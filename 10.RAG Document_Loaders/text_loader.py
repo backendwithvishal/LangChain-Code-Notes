458b2dfaa0f768586e4e410dbfc5f82884ff8cf5
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-15 19:31:54 +0530

    code update in file

--- 10.RAG Document_Loaders/text_loader.py
+++ 10.RAG Document_Loaders/text_loader.py
@@ -1,3 +1,7 @@
 from langchain_community.document_loaders import TextLoader
 
 loader = TextLoader('cricket.txt', encoding='utf-8')
+docus = loader.load()
+# print(docus)
+print(type(docus))
+# print(docus[0].page_content)
