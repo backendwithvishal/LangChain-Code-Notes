9cdf64f3df25e3db3f8b24bcbd8bfcdd0d11c71e
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-17 21:16:11 +0530

    small update in codebase

--- 10.RAG Document_Loaders/csv_loader.py
+++ 10.RAG Document_Loaders/csv_loader.py
@@ -2,6 +2,7 @@ from langchain_community.document_loaders import CSVLoader
 
 loader = CSVLoader(file_path = 'Social_Network_Ads.csv')
 
-data = loader.load()
+docs = loader.load()
 
-print(data[0])
+print(len(docs))
+print(docs[0])
