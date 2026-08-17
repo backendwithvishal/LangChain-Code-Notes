dc79acda392fe15f2c1f52a10ec5ecc501eddfb3
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-17 20:13:41 +0530

    small update in codebase

--- 10.RAG Document_Loaders/directory_loader.py
+++ 10.RAG Document_Loaders/directory_loader.py
@@ -6,7 +6,10 @@ loader = DirectoryLoader(
     loader_cls = PyPDFLoader
 )
 
-docs = loader.load()
+docs = loader.lazy_load()
 
-print(docs[325].page_content)
-print(len(docs))
+# print(docs[325].page_content)
+# print(len(docs))
+
+for document in docs:
+    print(document.metadata)
