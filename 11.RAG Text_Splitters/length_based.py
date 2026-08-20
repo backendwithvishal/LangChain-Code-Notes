f0aa3bf53de5994b4134c3b6876f810afa9eb617
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-20 07:27:12 +0530

    code added in new file

---
+++ 11.RAG Text_Splitters/length_based.py
@@ -0,0 +1,16 @@
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader('d1-curriculum.pdf')
+
+docs = loader.load()
+
+splitter = CharacterTextSplitter(
+    chunk_size = 100,
+    chunk_overlap = 0,
+    separator = ""
+)
+
+result = splitter.split_documents
+
+print(result)
